b72d93fe77f3ad150d91a965b4832bf574903061
wantroba <wantr0ba@hotmail.com>
2017-08-11 18:09:12 -0300

    Mensagem toast

--- app/src/main/java/com/example/wantroba/androidgit/MainActivity.java
+++ app/src/main/java/com/example/wantroba/androidgit/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity
         final View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"meu toast",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"meu segundo toast",Toast.LENGTH_SHORT).show();
             }
         };
 
